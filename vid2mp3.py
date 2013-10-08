bab1ac1e109212a4499ecaaba5822c69199f17e3
iamsudip <iamsudip@programmer.net>
2013-10-08 18:31:44 +0530

    handled a critical TypeError exception

--- vid2mp3.py
+++ vid2mp3.py
@@ -10,18 +10,29 @@ import sys
 from bs4 import BeautifulSoup
 import time
 
-def abc(s_url, sleep_time = 0):
+def abc(s_url, sleep_time = 10):
     try:
         s_url_soup = BeautifulSoup(requests.get(s_url).text, "xml")
+        global data
         data = { "url" : str(s_url_soup.downloadurl.contents[0]),
             "name" : str(s_url_soup.file.contents[0]),
-            "size" : str(s_url_soup.filesize.contents[0])
+            "size" : str(s_url_soup.filesize.contents[0]),
         }
         print "Downloading: %s" % data["name"]
-        return data
+        print "Fetching data from: %s" % data["url"]
+        try:
+            with open("%s" %data["name"], "wb") as fobj:
+                pass
+        except IOError:
+            data["name"] = "Rename_manually.mp3"
+        with open("%s" %data["name"], "wb") as fobj:
+            print "File size: " + data["size"]
+            response = requests.get(data["url"], stream=True)
+            copyfileobj(response.raw, fobj)
+        
     except AttributeError:
         time.sleep(sleep_time)
-        abc(s_url, sleep_time =+10)
+        abc(s_url, sleep_time =+ 10)
 
 def copyfileobj(fsrc, fdst, length=16*1024):
     """copy data from file-like object fsrc to file-like object fdst"""
@@ -34,23 +45,22 @@ def copyfileobj(fsrc, fdst, length=16*1024):
 def process_url(url):
     response = requests.post("http://www.listentoyoutube.com/cc/conversioncloud.php", data={"mediaurl": url})
     s_url = eval(response.text)['statusurl'].replace('\/', '/')
-    print s_url
-    download(abc(s_url))
+    content = abc(s_url)
+    return content
 
+'''
 def download(data):
     try:
         with open("%s" %data["name"], "wb") as fobj:
-            
             print "File size: " + data["size"]
             response = requests.get(data["url"], stream=True)
             copyfileobj(response.raw, fobj)
     except IOError:
         data["name"] = "Rename_manually.mp3"
         download(data)
+'''
 
 if __name__ == '__main__':
     data = process_url(sys.argv[1])
-    download(data)
-
 
 # http://www.listentoyoutube.com/process.php?url=http://www.youtube.com/watch?v=abcdef
